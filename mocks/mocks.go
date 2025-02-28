4034fd955425bb6203c0b454c3b92606f229d620
HariKrishnan S <159516754+HarryKrishnan@users.noreply.github.com>
2025-02-28 16:26:47 +0530

    Add files via upload

---
+++ mocks/mocks.go
@@ -0,0 +1,20 @@
+package mocks
+
+import (
+	"jwt-auth/models"
+)
+
+var Users = []models.User{
+	{
+		Username: "babugeet",
+		Password: "123",
+	},
+	{
+		Username: "abhinandh",
+		Password: "234",
+	},
+	{
+		Username: "w",
+		Password: "1",
+	},
+}
