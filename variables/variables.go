2f762be305c931d04274f08e549ff2d38f7f5ee5
HariKrishnan S <159516754+HarryKrishnan@users.noreply.github.com>
2025-02-28 16:30:03 +0530

    Add files via upload

---
+++ variables/variables.go
@@ -0,0 +1,11 @@
+package variables
+
+import "time"
+
+var CurrentDay = time.Now().Weekday()
+
+var DayColumnName = CurrentDay.String()
+
+// var DayColumnName = "Wednesday"
+
+var Today = time.Now().Format("2006-01-02")
